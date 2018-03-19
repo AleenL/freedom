4bb87c405816016d76d9a6e17c78ad34a5b26852
AleenL <784757555@qq.com>
2018-03-19 19:43:32 +0800

    从头再来

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import {BrowserRouter,Router,Switch,HashRouter,Match,Route,Link,hashHistory,IndexLink} from 'react-router-dom'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import BasicExample from './main'
-
-ReactDOM.render(
-    <BasicExample/>,
-    document.getElementById('root')
-);
